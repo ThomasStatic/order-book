b269da314a0d2255d0144991fe2d4ed548913193
Thomas Boyle <tsboyle10@gmail.com>
2026-08-03 01:20:13 -0400

    order book methods and members for asks/bids storage

--- include/order_book/order_book.hpp
+++ include/order_book/order_book.hpp
@@ -1,15 +1,32 @@
 #pragma once
 
-#include<unordered_map>
+#include <unordered_map>
+#include <map>
+#include <compare>
 
 #include "order_location.hpp"
+#include "price_level.hpp"
+#include "order.hpp"
 
 namespace order_book {
     
+    using Price = unsigned int;
+
     class OrderBook final {
+    private:
+        std::unordered_map<Price, OrderLocation> orderIndex;
+
+        std::map<Price, PriceLevel, std::greater<Price>> bids;
+        std::map<Price, PriceLevel, std::less<Price>> asks;
+
+        const Order& getBestBid() const;
+        const Order& getBestAsk() const;
+
+        bool hasBids() const;
+        bool hasAsks() const;        
+
     public:
         OrderBook() = default;
-        std::unordered_map<unsigned int, OrderLocation> orderIndex;
     };
     
 }  // namespace order_book
--- src/order_book.cpp
+++ src/order_book.cpp
@@ -1,1 +1,21 @@
 #include "order_book/order_book.hpp"
+
+namespace order_book {
+    const Order& OrderBook::getBestBid() const{
+        const PriceLevel& bestBidLevel = bids.begin()->second;
+        return bestBidLevel.getOldestOrder();
+    }
+
+    const Order& OrderBook::getBestAsk() const{
+        const PriceLevel& bestAskLevel = asks.begin()->second;
+        return bestAskLevel.getOldestOrder();
+    }
+
+    bool OrderBook::hasBids() const {
+        return !bids.empty();
+    }
+
+    bool OrderBook::hasAsks() const {
+        return !asks.empty();
+    }
+}
